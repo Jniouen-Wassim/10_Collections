3bac08762182ac7a35e9431b5932f789829f6aec
Jniouen-Wassim <Jniouenwassim@gmail.com>
2020-11-12 09:54:01 +0100

    commit

---
+++ doc.js
@@ -0,0 +1,55 @@
+const collection = [
+    {
+      title: 'Coulant au chocolat',
+      author: 'Edouard Sicot',
+      category: ['dessert'],
+      link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Le-vrai-coulant-au-chocolat-2280940',
+      description:  'Une recette simple et efficace pour tous les fans de chocolat.'
+    },
+
+    {
+        title: 'Cheesecake ',
+        author: 'Edouard Sicot',
+        category: ['dessert'],
+        link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Mon-super-banoffee-2258640',
+        description:  'On succombe à cette pâtisserie anglaise à base de banane et de de caramel.'
+      },
+
+      {
+        title: 'Sablés au parmesan ',
+        author: 'Jean Claude Amiel',
+        category: ['biscuit'],
+        link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Mon-super-banoffee-2258640',
+        description:  'Une idée simple et facile pour un apéro fait-maison. '
+      },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
