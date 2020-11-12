f66a4ae164a3f3d2ff9b1f3a3d621012bd077bfa
Jniouen-Wassim <Jniouenwassim@gmail.com>
2020-11-12 12:27:35 +0100

    commit

---
+++ collection.js
@@ -0,0 +1,82 @@
+const collection = [{
+        title: 'Coulant au chocolat',
+        author: 'Edouard Sicot',
+        category: ['dessert'],
+        link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Le-vrai-coulant-au-chocolat-2280940',
+        description: 'Une recette simple et efficace pour tous les fans de chocolat.'
+    },
+
+    {
+        title: 'Cheesecake ',
+        author: 'Edouard Sicot',
+        category: ['dessert'],
+        link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Mon-super-banoffee-2258640',
+        description: 'On succombe à cette pâtisserie anglaise à base de banane et de de caramel.'
+    },
+
+    {
+        title: 'Sablés au parmesan',
+        author: 'Jean Claude Amiel',
+        category: ['biscuit'],
+        link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Sables-au-parmesan-548053',
+        description: 'Une idée simple et facile pour un apéro fait-maison. '
+    },
+
+    {
+        title: 'Crème brûlée',
+        author: 'Jérôme Bilic',
+        category: ['dessert'],
+        link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Creme-brulee-facile-2894406',
+        description: 'Un délice que le monde entier nous envie !'
+    },
+
+    {
+        title: 'Cookies banane, avoine et chocolat noir',
+        author: 'Jérôme Bilic',
+        category: ['biscuit'],
+        link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Cookies-banane-flocons-d-avoine-et-chocolat-noir-3407042',
+        description: 'On succombe tous à ce biscuit à la banane, flocons et chocolat noir'
+    },
+
+    {
+        title: 'Gâteaux au chocolat',
+        author: 'Hanane',
+        category: ['dessert'],
+        link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Gateau-3-chocolats-979655',
+        description: 'Gâteau au 3 chocolats'
+    },
+
+    {
+        title: 'Cake au chocolat',
+        author: 'Getty',
+        category: ['dessert'],
+        link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Gateau-au-chocolat-facile-2080684',
+        description: 'Ultra-facile à cuisiner et à savourer, on fond pour cette recette de gâteau au chocolat, préparée avec 5 ingrédients à peine. Au menu, du chocolat noir (évidemment), du beurre, du sucre, des œufs et un soupçon de farine, suffisent pour préparer ce délice. Prête à fondre ?'
+    },
+
+    {
+        title: 'Mon super banoffee',
+        author: 'Edouard Sicot',
+        category: ['dessert'],
+        link: 'https://www.elle.fr/Elle-a-Table/Recettes-de-cuisine/Mon-super-banoffee-2258640',
+        description: 'On succombe à cette pâtisserie anglaise à base de banane et de de caramel.'
+    },
+
+    {
+        title: 'Salade d’orange à l’eau de fleur d’oranger',
+        author: 'Marjolaine Daguerre',
+        category: ['dessert'],
+        link: 'https://youtu.be/IymYbQnN-wc',
+        description: 'Le secret de cette recette ultra-rapide tient dans la découpe des oranges et dans le dosage subtil de l’eau de fleur d’oranger.'
+    },
+
+    {
+        title: 'Pâte à choux Thermomix',
+        author: 'Getty',
+        category: ['dessert'],
+        link: 'https://youtu.be/fTU168ttp_g',
+        description: 'Les secrets pour faire une pâte à choux parfaite avec son Thermomix.'
+    },
+
+]
+
